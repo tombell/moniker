1cde5e9bb748c74639cf2e5df5d07bc73b702b45
Tom Bell <tomb@tomb.io>
2018-09-02 22:51:10 +0100

    Add initial main.go and version.go for moniker command

---
+++ cmd/moniker/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/tombell/moniker"
+)
+
+const helpText = `usage: moniker [options] <directory>
+
+Format options:
+  --format   specify format of the file name to be changed to
+
+Special options:
+  --help     show this message, then exit
+  --version  show the version number, then exit
+`
+
+var (
+	format  = flag.String("format", "{artist} - {name}", "")
+	version = flag.Bool("version", false, "")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, helpText)
+	os.Exit(2)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "moniker %s (%s)\n", Version, Commit)
+		os.Exit(0)
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+	}
+
+	if err := moniker.Run(args[0], *format); err != nil {
+		panic(err)
+	}
+}
